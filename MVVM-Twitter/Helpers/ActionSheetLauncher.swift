0e5e5641bccdaa1d500efda4eede372113e84511
den0206 <mac.pop@mineo.jp>
2020-03-02 21:43:35 +0900

    Action Cell

--- MVVM-Twitter/Helpers/ActionSheetLauncher.swift
+++ MVVM-Twitter/Helpers/ActionSheetLauncher.swift
@@ -6,18 +6,131 @@
 //  Copyright © 2020 Yuuki sakai. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+private let reuseIdentifer = "ActionSheetCell"
 
 class ActionSheetLauncher : NSObject {
     
     private let user : User
+    private let tableview = UITableView()
+    private var window : UIWindow?
+    
+    private var tableViewHeight : CGFloat?
+    
+    //MARK: - Parts
+    
+    
     
     init(user : User) {
         self.user = user
         super.init()
+        
+        configureTableView()
     }
+    
+    private lazy var blackView : UIView = {
+        let view = UIView()
+        view.alpha = 0
+        view.backgroundColor = UIColor(white: 0, alpha: 0.5)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleDismiss))
+        view.addGestureRecognizer(tap)
+        return view
+    }()
+    
+    private lazy var footerView : UIView = {
+        
+        let view = UIView()
+        view.addSubview(cancelButton)
+        cancelButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        cancelButton.anchor(left : view.leftAnchor, right: view.rightAnchor,paddingLeft: 12,paddingRight: 12)
+        cancelButton.centerY(inView: view)
+        cancelButton.layer.cornerRadius = 50 / 2
+        
+        return view
+    }()
+    
+    private lazy var cancelButton : UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Cancel", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18)
+        button.setTitleColor(.black, for: .normal)
+        button.backgroundColor = .systemGroupedBackground
+        button.addTarget(self, action: #selector(handleDismiss), for: .touchUpInside)
+        return button
+    }()
+    
     
     func show() {
         print("AactionSheet\(user.fullname)")
+        
+        guard let window = UIApplication.shared.windows.first(where: {$0.isKeyWindow}) else {return}
+        
+        self.window = window
+        
+        window.addSubview(blackView)
+        blackView.frame = window.frame
+        
+        window.addSubview(tableview)
+        let height = CGFloat(3 * 60)
+        tableview.frame = CGRect(x: 0, y: window.frame.height , width: window.frame.width, height: height)
+        
+        // show black view
+        UIView.animate(withDuration: 0.5) {
+            self.blackView.alpha = 1
+            self.tableview.frame.origin.y -= height
+            
+        }
+        
+        
     }
+    
+    
+    func configureTableView() {
+        tableview.backgroundColor = .white
+        tableview.delegate = self
+        tableview.dataSource = self
+        
+        tableview.rowHeight = 40
+        tableview.separatorStyle = .none
+        tableview.layer.cornerRadius = 5
+        tableview.isScrollEnabled = false
+        
+        tableview.register(ActionSheetCell.self, forCellReuseIdentifier: reuseIdentifer)
+        
+    }
+    
+    //MARK: - Actions
+    @objc func handleDismiss() {
+        UIView.animate(withDuration: 0.5) {
+            self.blackView.alpha = 0
+            self.tableview.frame.origin.y += 300
+        }
+    }
+}
+
+extension ActionSheetLauncher : UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifer, for: indexPath) as! ActionSheetCell
+        
+        return cell
+    }
+    
+    //MARK: - Footer Delegate
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return footerView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return 60
+    }
+    
+    
 }
---
+++ MVVM-Twitter/View/ActionSheetCell.swift
@@ -0,0 +1,53 @@
+//
+//  ActionSheetCell.swift
+//  MVVM-Twitter
+//
+//  Created by 酒井ゆうき on 2020/03/02.
+//  Copyright © 2020 Yuuki sakai. All rights reserved.
+//
+
+import UIKit
+
+class ActionSheetCell : UITableViewCell {
+    
+    //MARK: - Parts
+    
+    private let optionImageView : UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.clipsToBounds = true
+        iv.image = #imageLiteral(resourceName: "twitter_logo_blue")
+        return iv
+    }()
+    
+    private let titleLabel : UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.text = "Test Option"
+        return label
+    }()
+    
+    
+    //MARK: - Ini
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        // anchor
+        addSubview(optionImageView)
+        optionImageView.centerY(inView: self)
+        optionImageView.anchor(left : leftAnchor, paddingLeft: 8)
+        optionImageView.setDimensions(width: 36, height: 36)
+        
+        addSubview(titleLabel)
+        titleLabel.centerY(inView: optionImageView)
+        titleLabel.anchor(left : optionImageView.rightAnchor, paddingLeft: 12)
+        
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
